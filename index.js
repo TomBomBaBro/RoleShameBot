f0b01d83aa86255c0e7c787b701c1e05488d7d5f
TomBomBaBro <thomas.l.bruning@gmail.com>
2023-01-26 16:33:25 -0600

    modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
-const roleID = '1063152302640345209' //Discord role ID goes here, can be accessed by using disocrd in developer mode.
+const roleID = '1063152302640345209' //Discord role ID goes here, can be accessed by using disocrd in developer mode. Robot Rock Test role
+const brownID = '938273307004907560' //Nectar Bears Brown Role
 
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
@@ -12,7 +13,7 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', async (message) => {
-    if (message.member.roles.cache.has(roleID)) {
+    if (message.member.roles.cache.has(roleID) || message.member.roles.cache.has(brownID)) {
         message.reply({
             content: 'https://tenor.com/Q9nz.gif',
         })
